e6bfa288e261349869eb6bdb4a7b8a6b588a66b1
Suman2198 <88475882+Suman2198@users.noreply.github.com>
2021-08-05 15:48:55 +0530

    Create 8.js

---
+++ 8.js
@@ -0,0 +1,12 @@
+const name = {firstName:'Philip',lastName:'Fry'};
+const details = {job:'Delivery Boy',employer:'Planet Express'};
+const emplo9yee = {...name,...details};
+console.log(employee);
+
+Output-
+  {
+     "firstName":"Philip",
+     "lastName":"Fry",
+     "job":"Delivery Boy",
+     "employer":"Planet Express"
+}
