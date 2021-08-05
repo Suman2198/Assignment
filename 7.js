6ea466e06ffb128d1cd165d99cdc5d9544a008f0
Suman2198 <88475882+Suman2198@users.noreply.github.com>
2021-08-05 15:41:47 +0530

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,15 @@
+const operatingSystem = {
+  name:'Ubuntu',
+  version:'18.4',
+  license:'open source'
+};
+const entries = Object.entries(operatingSystem);
+console.log(entries);
+
+Output-
+  [
+    ["name","Ubuntu"]
+    ["version",18.4]
+    ["license","open source"]
+  ]
+  
